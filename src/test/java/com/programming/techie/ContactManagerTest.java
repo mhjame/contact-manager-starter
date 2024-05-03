ba0fb208ddf8f4c5791d6aa8b579474daf831e76
21020068_DoanThiMinhHang <48597930+mhjame@users.noreply.github.com>
2024-05-03 09:57:36 +0700

    basic test and depedencies in pom.xml

---
+++ src/test/java/com/programming/techie/ContactManagerTest.java
@@ -0,0 +1,15 @@
+package com.programming.techie;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+import static org.junit.jupiter.api.Assertions.*;
+
+class ContactManagerTest {
+    @Test
+    public void shouldCreateContact() {
+        ContactManager contactManager = new ContactManager();
+        contactManager.addContact("John", "Doe", "0123456789");
+        Assertions.assertFalse(contactManager.getAllContacts().isEmpty());
+        Assertions.assertEquals(1, contactManager.getAllContacts().size());
+    }
+}
